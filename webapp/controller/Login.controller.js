5ee39f06c60f51f474cf62b8415b8836f6d2fd49
Dre <147393582+patsypatsyyy@users.noreply.github.com>
2025-04-18 14:57:36 +0800

    Implement login and homepage views, update routing, and add necessary UI5 libraries

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController, MessageBox) {
 	"use strict";
 
-	return BaseController.extend("personal.patsy.hotel.controller.Main", {
+	return BaseController.extend("personal.patsy.hotel.controller.Login", {
 		sayHello: function () {
 			MessageBox.show("Hello World!");
 		}
